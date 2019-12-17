61321afa8b449d50e0148bab79131ec3d0c2a893
Ankit Gupta <ankitgupta2oct@gmail.com>
2019-12-18 00:05:28 +0530

    Added Prototype pattern

---
+++ DesignPatternSample/Prototype/Executor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DesignPatternSample.Prototype
+{
+    class Executor
+    {
+        public Executor()
+        {
+            var person1 = new Person(new[] { "Ankit", "Vishal" }, new Address("Bangalore", 12));
+            var person2 = person1.DeepCopy();
+
+            person2.Names[0] = "Rohit";
+            person2.Address.HouseNumber = 44; 
+
+            Console.WriteLine(person1);
+            Console.WriteLine(person2);
+        }
+    }
+}
---
+++ DesignPatternSample/Prototype/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DesignPatternSample.Prototype
+{
+    class Person
+    {
+        public Person(string[] names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public string[] Names { get; set; }
+        public Address Address { get; set; }
+
+        public override string ToString()
+        {
+            return $"Names: {string.Join(" ", Names)}, Address: {Address}";
+        }
+    }
+}
