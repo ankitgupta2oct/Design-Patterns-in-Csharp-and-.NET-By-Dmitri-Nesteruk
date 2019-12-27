60034ddc1833ab61e0e52a4b2292b176f7bb3a3d
Ankit Gupta <ankitgupta2oct@gmail.com>
2019-12-27 12:46:21 +0530

    Added Adapter example

---
+++ DesignPatternSample/Adapter/Executor.cs
@@ -0,0 +1,13 @@
+﻿using DesignPatternSample.Builder.FacetedBuilder;
+using System;
+
+namespace DesignPatternSample.Adapter
+{
+    class Executor
+    {
+        public Executor()
+        {
+            new AdapterDemo();
+        }
+    }
+}
